5a3b0d49c1a8e78eb8145c594a906a7178e01f28
Costinel22 <malaiasi_costinel@yahoo.com>
2025-05-13 23:04:52 +0100

    al cincelea  update

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: process.env.NETLIFY ? '/' : '/invat-cu-glume/',
-    build: {
-        rollupOptions: {
-            external: [
-                'react',
-                'react-dom',
-                'react-router-dom'
-            ]
-        }
-    }
+    base: process.env.NETLIFY ? '/' : '/invat-cu-glume/'
+        // NU adăuga build.rollupOptions.external aici!
 })
