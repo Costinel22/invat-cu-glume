0e03e5e727b819f38aeefe4d1bbd6adf6f068181
Costinel22 <malaiasi_costinel@yahoo.com>
2025-05-13 22:58:56 +0100

    al patrulea  update

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: process.env.NODE_ENV === 'production' ?
-        '/NUME_REPO/' // Pentru GitHub Pages
-        :
-        '/', // Pentru Netlify/development
+    base: process.env.NETLIFY ? '/' : '/invat-cu-glume/',
     build: {
         rollupOptions: {
             external: [
-                // Adaugă module pe care vrei să le excludem din bundle
                 'react',
                 'react-dom',
                 'react-router-dom'
